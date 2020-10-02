47dd56cda0b45a7932ac7be0c37fc46aa08c7937
chlorox416 <chloechang416@gmail.com>
2020-10-02 01:33:16 -0400

    save

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -5,17 +5,44 @@ require 'tty-prompt'
 
 class Cli 
         def initialize()
-            @prompt = TTY::Prompt.new  
+            @prompt = TTY::Prompt.new
+            # pastel = Pastel.new.white.on_cyan.detach  
         end
         
         def welcome
             puts "What is your name?"    
             name = gets
-            p "Hello there #{name}!!"
+            print "Hello there #{name}"
             self.select
         end
 
         def select
+            
+            user_select = @prompt.select("What would you like to do?") do |menu|
+                    menu.choice "Browse by style", 1
+                    menu.choice "Browse by price point", 2
+                    menu.choice "See all brands", 3
+                    menu.choice "Abort!", 4
+            end
+
+            if user_select == 1
+                self.browse_by_styes
+            elsif user_select == 2
+                self.browse_by_price
+            elsif user_select == 3
+                puts Brand.all.map{|n|n.brand}
+                @prompt.keypress("-- Press any key to return --")
+                self.select
+            elsif user_select == 4
+                p "leave this!"
+                p "goodbye! :) "
+                exit
+
+            end
+        end
+
+
+        def browse_by_styes
             user_select = @prompt.select("What style would you like?") do |menu|
                     menu.choice "classic", 1
                     menu.choice "minimal", 2
@@ -24,15 +51,15 @@ class Cli
             end
             if user_select == 1
                 puts Brand.all.select{|brand|brand.style == "classic"}.map{|n|n.brand}
-                @prompt.keypress("Press any key to return")
+                @prompt.keypress("-- Press any key to return --")
                 self.select
             elsif user_select == 2
                 puts Brand.all.select{|brand|brand.style == "minimal"}.map{|n|n.brand}
-                @prompt.keypress("Press any key to return")
+                @prompt.keypress("-- Press any key to return --")
                 self.select
             elsif user_select == 3
-                puts Brand.all.select{|brand|brand.style == "streetwear"}.map{|n|n.brand}
-                @prompt.keypress("Press any key to return")
+                puts Brand.all.select{|brand|brand.style == "streetwear"}.map{|n|n.brand} #&& n.price
+                @prompt.keypress("-- Press any key to return --")
                 self.select
             elsif user_select == 4
                 p "leave this!"
@@ -40,7 +67,40 @@ class Cli
                 exit
                 
             end
-        end
+        end  
+        
+        
+        def browse_by_price
+            user_select = @prompt.select("What price point would you like?") do |menu|
+                    menu.choice "$", 1
+                    menu.choice "$$", 2
+                    menu.choice "$$$", 3
+                    menu.choice "nevermind!", 4
+            end
+            if user_select == 1
+                puts Brand.all.select{|brand|brand.price_point == "$"}.map{|n|n.brand}
+                @prompt.keypress("-- Press any key to return --")
+                self.select
+            elsif user_select == 2
+                puts Brand.all.select{|brand|brand.price_point == "$$"}.map{|n|n.brand}
+                @prompt.keypress("-- Press any key to return --")
+                self.select
+            elsif user_select == 3
+                puts Brand.all.select{|brand|brand.price_point == "$$$"}.map{|n|n.brand}
+                @prompt.keypress("-- Press any key to return --")
+                self.select
+            elsif user_select == 4
+                p "leave this!"
+                p "goodbye! :) "
+                exit
+                
+            end
+        end  
+       
+   
+
+
+        
 end      
 
         
